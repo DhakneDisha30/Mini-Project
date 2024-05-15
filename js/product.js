15513114d2ca4966df0b6a936f8eae1bdd187766
DhakneDisha30 <149703548+DhakneDisha30@users.noreply.github.com>
2024-05-15 16:25:09 +0530

    product.js

--- js/product.js
+++ js/product.js
@@ -1,26 +1,3 @@
-// const productImages = document.querySelectorAll(".product-images img"); // selecting all image thumbs
-// const productImageSlide = document.querySelector(".image-slider"); // seclecting image slider element
-
-// let activeImageSlide = 0; // default slider image
-
-// productImages.forEach((item, i) => { // loopinh through each image thumb
-//     item.addEventListener('click', () => { // adding click event to each image thumbnail
-//         productImages[activeImageSlide].classList.remove('active'); // removing active class from current image thumb
-//         item.classList.add('active'); // adding active class to the current or clicked image thumb
-//         productImageSlide.style.backgroundImage = `url('${item.src}')`; // setting up image slider's background image
-//         activeImageSlide = i; // updating the image slider variable to track current thumb
-//     })
-// })
-// const sizeBtns = document.querySelectorAll('.size-radio-btn'); // selecting size buttons
-// let checkedBtn = 0; // current selected button
-
-// sizeBtns.forEach((item, i) => { // looping through each button
-//     item.addEventListener('click', () => { // adding click event to each 
-//         sizeBtns[checkedBtn].classList.remove('check'); // removing check class from the current button
-//         item.classList.add('check'); // adding check class to clicked button
-//         checkedBtn = i; // upading the variable
-//     })
-// })
 
 let openShopping = document.querySelector('.shopping');
 let closeShopping = document.querySelector('.closeShopping');
@@ -169,7 +146,7 @@ function initApp(){
         newDiv.innerHTML = `
             <img src="img/${value.image}">
             <div class="title">${value.name}</div>
-            <div class="price">${value.price.toLocaleString()}</div>
+            <div class="price">RS ${value.price.toLocaleString()}</div>
             <button onclick="addToCard(${key})">Add To Card</button>`;
         list.appendChild(newDiv);
     })
@@ -195,7 +172,7 @@ function reloadCard(){
             newDiv.innerHTML = `
                 <div><img src="img/${value.image}"/></div>
                 <div>${value.name}</div>
-                <div>${value.price.toLocaleString()}</div>
+                <div>Rs ${value.price.toLocaleString()}</div>
                 <div>
                     <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
                     <div class="count">${value.quantity}</div>
